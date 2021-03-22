6dd8902dbc5ab12607c317cd37bb9ffb0869201f
Nick van Goor <nick.vangoor@danaher.com>
2021-03-22 13:57:31 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Clustering coding challenge used in Danaher Digital hiring',
+    author='Danaher Digital',
+    license='',
+)
